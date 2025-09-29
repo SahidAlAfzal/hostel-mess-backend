5d42433028612cd47afa2d52d91b736322591477
sahid al afzal <sahidalafzal@Sahids-MacBook-Air.local>
2025-09-29 16:11:16 +0530

    Feat: Update database config to use environment variables

--- app/database.py
+++ app/database.py
@@ -5,6 +5,15 @@ import os
 # Supabase gives you DATABASE_URL (set it in Render's environment variables)
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+
+if not DATABASE_URL:
+    # fallback for local dev if DATABASE_URL not set
+    DB_NAME = os.getenv("DB_NAME", "hosteldb")
+    DB_USER = os.getenv("DB_USER", "postgres")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "1234")
+    DB_HOST = os.getenv("DB_HOST", "localhost")
+    DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
+
 if not DATABASE_URL:
     raise Exception("DATABASE_URL environment variable not set!")
 
