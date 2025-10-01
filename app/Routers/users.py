8c2aa12a1a10720ea856fbc31850b5481a003037
sahid al afzal <sahidalafzal@Sahids-MacBook-Air.local>
2025-10-01 13:37:08 +0530

    Delete User feature is added

--- app/Routers/users.py
+++ app/Routers/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter,Depends,HTTPException,status
+from fastapi import APIRouter,Depends,HTTPException,status,Response
 from typing import List
 from ..database import get_db_connection
 from .. import schemas,oauth2
@@ -38,4 +38,41 @@ def update_convenor(user_id: int, role_update: schemas.UserRoleUpdate, conn = De
             raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR,detail=f"Database Error : {e}")
         
     return updated_user
+
+
+
+
+#-----------------------------------------------------DELETE USERS----------------------------------------------------------#
+@router.delete("/{user_id}",status_code=status.HTTP_204_NO_CONTENT)
+def delete_user(user_id : int, conn = Depends(get_db_connection),current_user: dict = Depends(oauth2.require_mess_committee_role)):
+
+    # Security Check 1: Prevent an admin from deleting their own account.
+    if user_id == current_user['id']:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Action not allowed: You cannot delete your own account.")
+
+
+    with conn.cursor() as c:
+        c.execute("SELECT role FROM users WHERE id=%s",(user_id,))
+        user_to_delete = c.fetchone()
+
+    if not user_to_delete:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"User with id {user_id} not found!")
+
+    if user_to_delete['role'] == 'mess_committee':
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail=f"Action not allowed: A mess committee member cannot be deleted")
+
+    query = "DELETE FROM users WHERE id=%s;"
+
+    with conn.cursor() as c:
+        try:
+            c.execute(query,(user_id,))
+            conn.commit()
+        except Exception as e:
+            conn.rollback()
+            raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR,detail=f"Database Error : {e}")
+        
+        # Return a 204 No Content response on successful deletion.
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
     
