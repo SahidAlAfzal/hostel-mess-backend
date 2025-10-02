9d85e16d0928267d02d3f3790d449e737368fefd
sahid al afzal <sahidalafzal@Sahids-MacBook-Air.local>
2025-10-02 22:39:25 +0530

    added delete notice feature

--- app/Routers/notice.py
+++ app/Routers/notice.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter,Depends,HTTPException,status
+from fastapi import APIRouter,Depends,HTTPException,status,Response
 from .. import database,schemas,oauth2
 import psycopg2
 from typing import List
@@ -40,6 +40,36 @@ def get_all_notice(conn = Depends(database.get_db_connection),current_user = Dep
         notices = c.fetchall()
 
     return notices
+
+
+#-------------------------------------------------DELETE NOTICE------------------------------------------------------#
+@router.delete("/{notice_id}",status_code=status.HTTP_204_NO_CONTENT)
+def delete_notice(notice_id: int,conn = Depends(database.get_db_connection),current_user: dict = Depends(oauth2.require_admin_role)):
+    with conn.cursor() as c:
+        c.execute("SELECT posted_by_user_id FROM notices WHERE id=%s",(notice_id,))
+        postman = c.fetchone()
+
+    if not postman:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Notice not found!")
+    
+    postman_id = postman['posted_by_user_id']
+
+    if current_user['role'] == 'convenor' and postman_id != current_user['id']:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+    
+
+    query = "DELETE FROM notices WHERE id=%s"
+
+    with conn.cursor() as c:
+        try:
+            c.execute(query,(notice_id,))
+            conn.commit()
+        except Exception as e:
+            conn.rollback()
+            raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR,detail=f"Database Error : {e}")
+        
+        # Return a 204 No Content response on successful deletion.
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
 
     
         
