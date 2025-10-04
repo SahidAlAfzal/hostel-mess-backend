a4143e685a3371db0af52ecd2ebf96c16d60a1e6
sahid al afzal <sahidalafzal@Sahids-MacBook-Air.local>
2025-10-04 10:33:44 +0530

    Fix the users.py

--- app/Routers/users.py
+++ app/Routers/users.py
@@ -9,7 +9,7 @@ router = APIRouter(prefix="/users",tags=["User Management"])
 #-----------------------------------Get All Users' Info--------------------------------------#
 @router.get("/",response_model=List[schemas.UserOut])
 def get_all_users(conn = Depends(get_db_connection), current_user: dict = Depends(oauth2.require_mess_committee_role)):
-    query = "SELECT id, name, email, room_number, role, is_active, created_at FROM users ORDER BY id;"
+    query = "SELECT id, name, email, room_number, role, is_active, is_mess_active, created_at FROM users ORDER BY id;"
 
     with conn.cursor() as c:
         c.execute(query)
@@ -21,16 +21,25 @@ def get_all_users(conn = Depends(get_db_connection), current_user: dict = Depend
 #---------------------------------UPDATE CONVENOR-------------------------------#
 @router.patch("/{user_id}",response_model=schemas.UserOut)
 def update_convenor(user_id: int, role_update: schemas.UserRoleUpdate, conn = Depends(get_db_connection), current_user: dict = Depends(oauth2.require_mess_committee_role)):
+    with conn.cursor() as cur:
+        cur.execute("SELECT role FROM users WHERE id=%s",(user_id,))
+        check_user = cur.fetchone()
+
+    if not check_user:
+        raise HTTPException(status.HTTP_404_NOT_FOUND,detail=f"User with id {user_id} not found")
+    
+    if check_user['role'] == 'mess_committee':
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail=f"You can't change the role of mess committee member")
+
+
+
     query = """UPDATE users SET role=%s
             WHERE id=%s
-            RETURNING id, name, email, room_number, role, is_active, created_at;"""
+            RETURNING id, name, email, room_number, role, is_active, is_mess_active, created_at;"""
     with conn.cursor() as c:
         try:
             c.execute(query,(role_update.role.value,user_id))
             updated_user = c.fetchone()
-
-            if not updated_user:
-                raise HTTPException(status.HTTP_404_NOT_FOUND,detail=f"User with id {user_id} not found")
         
             conn.commit()
         except Exception as e:
@@ -76,6 +85,7 @@ def delete_user(user_id : int, conn = Depends(get_db_connection),current_user: d
         
         # Return a 204 No Content response on successful deletion.
         return Response(status_code=status.HTTP_204_NO_CONTENT)
+
 
 
 #----------------------------------------------------UPDATE MESS STATUS------------------------------------------------------#
